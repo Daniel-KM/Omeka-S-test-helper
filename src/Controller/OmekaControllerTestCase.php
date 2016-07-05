a75df6feaa3e80c91ad80ff74cbfc50eac0752fc
Julian Maurice <julian.maurice@biblibre.com>
2016-07-05 21:01:39 +0200

    Initial commit

---
+++ src/Controller/OmekaControllerTestCase.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace OmekaTestHelper\Controller;
+
+use Omeka\Test\AbstractHttpControllerTestCase;
+
+abstract class OmekaControllerTestCase extends AbstractHttpControllerTestCase
+{
+    public function postDispatch($url, $data)
+    {
+        return $this->dispatch($url, HttpRequest::METHOD_POST, $data);
+    }
+
+    protected function resetApplication()
+    {
+        $this->application = null;
+    }
+
+    protected function getServiceLocator()
+    {
+        return $this->getApplication()->getServiceManager();
+    }
+
+    protected function api()
+    {
+        return $this->getServiceLocator()->get('Omeka\ApiManager');
+    }
+
+    protected function login($email, $password)
+    {
+        $serviceLocator = $this->getServiceLocator();
+        $auth = $serviceLocator->get('Omeka\AuthenticationService');
+        $adapter = $auth->getAdapter();
+        $adapter->setIdentity($email);
+        $adapter->setCredential($password);
+        return $auth->authenticate();
+    }
+
+    protected function loginAsAdmin()
+    {
+        $this->login('admin@example.com', 'root');
+    }
+}
