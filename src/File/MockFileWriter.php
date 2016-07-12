2dce92784bfee504072eb48ee2d890ddcf5f3388
efalcy <efalcy@afi-sa.fr>
2016-07-12 14:57:44 +0200

    put trace for debug on travis

--- src/File/MockFileWriter.php
+++ src/File/MockFileWriter.php
@@ -41,6 +41,9 @@ class MockFileWriter {
 
     public function glob($pattern, $flag=0) {
         $file=str_replace('{.*,.,\,,}','.png',$pattern);
+        echo 'file = '.$file.'\n';
+        var_dump($files);
+
         return $this->fileExists($file);
     }
 
