273de64af06e59c61f1d65c40cecca1c6d3ab873
Julian Maurice <julian.maurice@biblibre.com>
2016-09-12 16:48:42 +0200

    Fix MockFileWriter

--- src/File/MockFileWriter.php
+++ src/File/MockFileWriter.php
@@ -1,9 +1,11 @@
 <?php
 namespace OmekaTestHelper\File;
+
 class MockFileWriter {
     protected $files = [];
+
     public function moveUploadedFile($source,$destination) {
-        array_diff($this->files,[$source]);
+        $this->files = array_diff($this->files, [$source]);
         $this->files[]=$destination;
         return true;
     }
@@ -12,9 +14,7 @@ class MockFileWriter {
     }
 
     public function fileExists($path) {
-        if (in_array($path,$this->files))
-            return true;
-        return false;
+        return in_array($path, $this->files);
     }
 
     public function is_writable($path) {
@@ -30,8 +30,8 @@ class MockFileWriter {
     }
 
     public function rename($path, $destination) {
-        array_diff($this->files,[$path]);
-        $this->files[]=$destination;
+        $this->files = array_diff($this->files, [$path]);
+        $this->files[] = $destination;
         return true;
     }
 
@@ -40,8 +40,8 @@ class MockFileWriter {
     }
 
     public function glob($pattern, $flag=0) {
-        $file=str_replace('{.*,.,\,,}','.png',$pattern);
-        return $this->fileExists($file);
+        $file = str_replace('{.*,.,\,,}', '.png', $pattern);
+        return $this->fileExists($file) ? [$file] : [];
     }
 
     public function addFile($path) {
